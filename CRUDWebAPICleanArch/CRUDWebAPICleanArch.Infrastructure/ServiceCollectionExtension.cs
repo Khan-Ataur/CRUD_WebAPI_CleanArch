8aa8f1c50a31634cc2d841ebecbe5477a1f76966
Khan Ataur Rahman <khanataur.cse.pstu@gmail.com>
2024-02-07 12:13:55 +0600

    Clean Architecture Structure ready for CRUD operation
    Clean Architecture Structure ready for CRUD operation

---
+++ CRUDWebAPICleanArch/CRUDWebAPICleanArch.Infrastructure/ServiceCollectionExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDWebAPICleanArch.Infrastructure
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            //services.AddTransient<IProductRepository, ProductRepository>();
+            //services.AddTransient<IContactRepository, ContactRepository>();
+            //services.AddTransient<IUnitOfWork, UnitOfWork>();
+        }
+    }
+}
