c58d60d5715f6124e144e8821b41f61696daccb6
Khan Ataur Rahman <khanataur.jp@gmail.com>
2024-02-17 23:51:16 +0600

    product VM add for api response

---
+++ CRUDWebAPICleanArch/CRUDCoreWebApp/Models/ProductVM.cs
@@ -0,0 +1,9 @@
+﻿namespace CRUDCoreWebApp.Models
+{
+    public class ProductVM
+    {
+        public bool success { get; set; }
+        public string message { get; set; }
+        public List<Product> result { get; set; }
+    }
+}
